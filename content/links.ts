6712dc1f4e7bc5e6af74ea2c953daf7397914dd9
bkandh30 <kandhari.bhavya20@gmail.com>
2025-10-15 18:08:10 -0700

    Created links content module

---
+++ content/links.ts
@@ -0,0 +1,6 @@
+export const links = {
+	resume: "/resume", // placeholder text
+	github: "https://github.com/bkandh30",
+	linkedin: "https://www.linkedin.com/in/bhavyakandhari",
+	email: "mailto:bhavya.kandhari.eng@gmail.com"
+} as const;
